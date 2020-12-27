84a6f226d887534e5f9487ed4aea1c2c3a135873
javier.parisr <javier.parisr@udc.es>
2020-12-27 19:30:10 +0100

    asteroides WIP
    Former-commit-id: 1c8e330a36a452a1c71f1e309ff2e4232044df37

---
+++ Loophole/Assets/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float shootRate;
+    private float nextShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextShoot)
+        {
+            nextShoot = Time.time + shootRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+}
+
