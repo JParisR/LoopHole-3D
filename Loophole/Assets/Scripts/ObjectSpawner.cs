db0f50da6e7198fde595a9b714c458f183697f81
Xián García Veiga <x.veiga@udc.es>
2021-01-02 06:06:59 +0100

    Mejoras en spawn de objetos, textura alternativa para naves enemigas

--- Loophole/Assets/Scripts/ObjectSpawner.cs
+++ Loophole/Assets/Scripts/ObjectSpawner.cs
@@ -2,53 +2,87 @@
 
 public class ObjectSpawner : MonoBehaviour
 {
-    public GameObject[] asteroidTypes;
-    public Vector3 spawnBoxSize;
-    public Vector3 initialSpeed;
-    public Vector3 minimumSpeed;
-    public Vector3 maxRandomTranslationSpeed;
-    public Vector3 maxRandomRotationSpeed;
-    public float despawnDistance;
-    public int itemCount;
+    public GameObject[] asteroidTypes; // Array con tipos de objetos a crear
+    public Vector3 spawnBoxSize; // Tamaño de la caja en la que se crean los objetos
+    public Vector3 displacementSpeed; // Velocidad de los objetos respecto a la nave.
+    public Vector3 minimumSpeed; // Velocidad mínima de los objetos para considerar que están "bloqueados" (ver ensureObjectIsMoving).
+    public Vector3 maxRandomTranslationSpeed; // Velocidad máxima aleatoria al crear el objeto
+    public Vector3 maxRandomRotationSpeed; // Rotación máxima aleatoria al crear el objeto
+    public float despawnDistance; // Distancia de destrucción del objeto desde el origen del spawner.
+    public int itemCount; // Límite de objetos simulados concurrentemente.
 
-    private int spawnedObjects = 0;
+    private int spawnedObjects = 0; // Contador de objetos simulados
+
+    void Update()
+    {
+        // No tengo claro si se debe llamar en Update o en FixedUpdate.
+        // En FixedUpdate da "tirones" al no coincidir la frecuencia de gráficos con la de físicas.
+        updatePositions();
+    }
 
     void FixedUpdate()
     {
         if (spawnedObjects < itemCount)
-        {
-            foreach (GameObject o in asteroidTypes)
-            {
-                if (spawnedObjects < itemCount)
-                {
-                    GameObject instance = Instantiate(o, transform.position + randomVector3(spawnBoxSize), randomRotation());
+            spawnObjects();
+        //updatePositions();
+    }
 
-                    instance.transform.SetParent(transform);
-                    Rigidbody rb = instance.GetComponent<Rigidbody>();
+    // Método para variar la velocidad de desplazamiento.
+    // Por ejemplo, para permitir a la nave ir más rápido temporalmente.
+    public void setSpeed(Vector3 speed)
+    {
+        displacementSpeed = speed;
+    }
 
-                    rb.AddTorque(randomVector3(maxRandomRotationSpeed), ForceMode.VelocityChange);
-                    rb.AddForce(initialSpeed + randomVector3(maxRandomTranslationSpeed), ForceMode.VelocityChange);
-
-                    spawnedObjects++;
-                }
-            }
-        }
+    private void updatePositions()
+    {
         foreach (Transform child in transform)
         {
-            //TODO: Inhabilitar físicas rigidbody, luego mover vector de posición del objeto y por último llamar a update de rigidbody.
-            Rigidbody rb = child.GetComponent<Rigidbody>();
-            if ((Vector3.Angle(rb.velocity, initialSpeed) > 45) && (Vector3.SqrMagnitude(rb.velocity) < Vector3.SqrMagnitude(initialSpeed)))
+            child.localPosition += Time.deltaTime * displacementSpeed;
+            destroyIfDistant(child);
+        }
+    }
+
+    // Destruye el objeto si se aleja más de la distancia límite.
+    private void destroyIfDistant(Transform t)
+    {
+        // Si el objeto se aleja más de despawnDistance desde el origen del spawn, lo destruye y decrementa el contador.
+        if (t.position.magnitude - transform.position.magnitude > despawnDistance)
+        {
+            Destroy(t.gameObject);
+            spawnedObjects--;
+        }
+    }
+
+    // Crea objetos si todavía no se llegó al número límite
+    private void spawnObjects()
+    {
+        // Para cada objeto en los tipos definidos
+        foreach (GameObject o in asteroidTypes)
+        {
+            // Si todavía no se ha llegado al número máximo de objetos, crea uno.
+            // De esta manera se van creando progresivamente uno por fotograma y no todos juntos.
+            // Pendiente de retocar dependiendo de qué manera queremos que aparezcan los objetos.
+            if (spawnedObjects < itemCount)
             {
-                rb.AddForce(initialSpeed, ForceMode.VelocityChange);
-            }
-            if (child.position.magnitude - transform.position.magnitude > despawnDistance)
-            {
-                Destroy(child.gameObject);
-                spawnedObjects--;
+                // Crea una instancia del objeto con posición aleatoria
+                GameObject instance = Instantiate(o, transform.position + randomVector3(spawnBoxSize), randomAngle());
+
+                // La coloca como hijo del spawner
+                instance.transform.SetParent(transform);
+
+                // Obtiene rigidbody para añadir una velocidad y rotación aleatorias
+                Rigidbody rb = instance.GetComponent<Rigidbody>();
+                rb.AddTorque(randomVector3(maxRandomRotationSpeed), ForceMode.VelocityChange);
+                rb.AddForce(randomVector3(maxRandomTranslationSpeed), ForceMode.VelocityChange);
+
+                // Aumenta contador de objetos simulados
+                spawnedObjects++;
             }
         }
     }
 
+    // Genera un Vector3 con valores aleatorios en el rango [-limit, limit]
     private Vector3 randomVector3(Vector3 limit)
     {
         return new Vector3(
@@ -58,7 +92,8 @@ public class ObjectSpawner : MonoBehaviour
                         );
     }
 
-    private Quaternion randomRotation()
+    // Genera un Quaternion con ángulos aleatorios
+    private Quaternion randomAngle()
     {
         return Quaternion.Euler(
                             Random.Range(0, 360),
