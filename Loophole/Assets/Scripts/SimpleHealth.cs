e6654b1240b3cdb42d7b03e84adde16217a2f15a
Barbara <barbara@central.commcenter.es>
2021-01-05 10:54:38 +0100

    b_merge_1
    Former-commit-id: 0e640d95dea42ccb7155b064b7c0a67ea5bdc2dc

---
+++ Loophole/Assets/Scripts/SimpleHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleHealth : MonoBehaviour
+{
+    /*
+    public Slider healthBar;
+    SpaceshipBehaviour playerHealth;
+
+    void Start(){
+        playerHealth = GameObject.FindWithTag("Player Spaceship").GetComponent<SpaceshipBehaviour>();
+    }
+    void Update(){
+        if (healthBar.value != playerHealth.health)
+        {
+            Debug.Log("cambiamos health");
+        }    
+        healthBar.value = playerHealth.health;
+    }
+    */
+}
---
+++ Loophole/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Waypoint : MonoBehaviour
+{
+    private Image iconImage;
+    private Text distanceText;
+
+    public Transform player;
+    public Transform target;
+    public Camera cam;
+
+    public float closeEnoughDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iconImage = GetComponent<Image>();
+        distanceText = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            GetDistance();
+            CheckOnScreen();
+        }
+    }
+
+    void GetDistance()
+    {
+        float dist = Vector3.Distance(player.position, target.position);
+        distanceText.text = dist.ToString("f1") + "m";
+
+        if (dist < closeEnoughDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void CheckOnScreen()
+    {
+        float thing = Vector3.Dot((target.position - cam.transform.position).normalized, cam.transform.forward);
+
+        if (thing <= 0)
+        {
+            ToggleUI(false);
+
+        }
+        else
+        {
+            ToggleUI(true);
+            Vector3 screenPos = cam.WorldToScreenPoint(target.position);
+            transform.position = screenPos;
+            //transform.position = new Vector3(screenPos.x, screenPos.y, screenPos.z);
+            //Debug.Log("target is " + screenPos.x + " pixels from the left");
+        }
+    }
+
+    void ToggleUI(bool _value)
+    {
+        iconImage.enabled = _value;
+        distanceText.enabled = _value;
+    }
+}
