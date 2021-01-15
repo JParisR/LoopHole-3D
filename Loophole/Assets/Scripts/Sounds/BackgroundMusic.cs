0d49f6acd9eb3c7fc5d1832363500183a1bae626
Xián García Veiga <x.veiga@udc.es>
2021-01-15 16:56:19 +0100

    Música de fondo
    Former-commit-id: 64dada6c837704d9ab4ce35c88b5367bc74bdd9b

---
+++ Loophole/Assets/Scripts/Sounds/BackgroundMusic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BackgroundMusic : MonoBehaviour
+{
+    private static BackgroundMusic instance = null;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else if (instance == this)
+        {
+            return;
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+}
